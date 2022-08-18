8372604d6f9f7c745ea2c6bad779d4fdf9a2a198
Kwabena Agyeman <kwabena@delt.ai>
2022-08-18 15:28:03 +0000

    Setup logic to retrieve session storage
    Setup logic to retrieve session storage and store it in Component state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
+
+// Styles
+import './App.css';
+
+// Paticles Package
 import Particles from 'react-particles';
 import { loadFull } from 'tsparticles';
-import './App.css';
-import Nav from './Components/Nav';
 import particlesOptions from './particles.json';
 
+// Components
+import Nav from './Components/Nav';
+import { Container } from 'react-bootstrap';
+
 function App() {
+  const [publicAccessKey, setPublicAccessKey] = useState(
+    sessionStorage.getItem('inSidedProjectPAT')
+  );
   const particlesInit = useCallback((main) => {
     loadFull(main);
   }, []);
@@ -14,6 +24,13 @@ function App() {
     <div className='App'>
       <Particles options={particlesOptions} init={particlesInit} />
       <Nav />
+      <Container>
+        {!publicAccessKey ? (
+          <p>Input form goes here</p>
+        ) : (
+          <p>Github meun goes here</p>
+        )}
+      </Container>
     </div>
   );
 }
